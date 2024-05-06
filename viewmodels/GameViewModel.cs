8013550359d1ec8482c062d0193dc9c586d1de1f
SledDev <sleddev@asd.dog>
2024-05-06 12:20:01 +0200

    background grid

--- viewmodels/GameViewModel.cs
+++ viewmodels/GameViewModel.cs
@@ -5,4 +5,7 @@ namespace TwoEleven.viewmodels;
 public partial class GameViewModel : ObservableObject {
     [ObservableProperty]
     private string something = "asd";
+
+    [ObservableProperty]
+    private int score;
 }
