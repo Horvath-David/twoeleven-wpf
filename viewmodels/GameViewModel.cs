1bd6a7a2683f679bd3c8027a4660c23d493ff0d1
SledDev <sleddev@asd.dog>
2024-05-09 01:01:25 +0200

    pretty much done

--- viewmodels/GameViewModel.cs
+++ viewmodels/GameViewModel.cs
@@ -28,19 +28,14 @@ public partial class GameViewModel : ObservableObject {
 
     private bool ProcessMove(Direction dir, bool dryRun = false) {
         var didMerge = false;
-
-        var didMove = PushAll(dir, dryRun);
-        var didChange = didMove || didMerge;
-        if (didChange && !dryRun) SpawnNew();
-        return didChange;
-    }
-
-    private bool PushAll(Direction dir, bool dryRun = false) {
         var didMove = false;
 
-        // Removing unneeded tiles
+        // Removing unneeded tiles, flags
         foreach (var tile in Tiles.ToList().Where(tile => tile.IsDeleted)) {
             Tiles.Remove(tile);
+        }
+        foreach (var tile in Tiles.ToList().Where(tile => tile.IsMerged)) {
+            tile.IsMerged = false;
         }
 
         var deltaX = dir switch {
@@ -61,7 +56,7 @@ public partial class GameViewModel : ObservableObject {
             Direction.Down => Tiles.OrderBy(t => -t.Y),
             _ => new List<TileViewModel>().Order()
         }).ToList();
-        
+
         // Preprocessing for animations
         foreach (var tile in sorted) {
             tile.IsMoving = false;
@@ -69,26 +64,58 @@ public partial class GameViewModel : ObservableObject {
             tile.PrevPhysicalY = tile.PhysicalY;
         }
         
-        // // Merging
-        // foreach (var tile in sorted) {
-        //     // Console.WriteLine($"Starting: {tile.X}, {tile.Y}");
-        //     // Console.WriteLine($"Deltas: {deltaX}, {deltaY}");
-        //     while (Tiles.FirstOrDefault(t => tile.X + deltaX == t.X && tile.Y + deltaY == t.Y) == null
-        //            && tile.X + deltaX < 4 && tile.X + deltaX >= 0 && tile.Y + deltaY < 4 && tile.Y + deltaY >= 0) {
-        //         didMove = true;
-        //         if (dryRun) break;
-        //         tile.X += deltaX;
-        //         tile.Y += deltaY;
-        //     }
-        //     // Console.WriteLine($"After: {tile.X}, {tile.Y}");
-        // }
-        // // Console.WriteLine("---------");
+        // Pre-pushing
+        foreach (var tile in sorted) {
+            // Console.WriteLine($"Starting: {tile.X}, {tile.Y}");
+            // Console.WriteLine($"Deltas: {deltaX}, {deltaY}");
+            while (Tiles.FirstOrDefault(t => tile.X + deltaX == t.X && tile.Y + deltaY == t.Y && !t.IsDeleted) == null
+                   && tile.X + deltaX < 4 && tile.X + deltaX >= 0 && tile.Y + deltaY < 4 && tile.Y + deltaY >= 0) {
+                didMove = true;
+                if (dryRun) break;
+                tile.X += deltaX;
+                tile.Y += deltaY;
+            }
+            // Console.WriteLine($"After: {tile.X}, {tile.Y}");
+        }
+        // Console.WriteLine("---------");
+
+        // Merging
+        foreach (var tile in sorted) {
+            var adjacent = Tiles.FirstOrDefault(t => tile.X - deltaX == t.X && tile.Y - deltaY == t.Y);
+            if (adjacent == null || adjacent.Value != tile.Value) continue;
+            // Console.WriteLine($"Deltas: {deltaX}, {deltaY}");
+            // Console.WriteLine($"Starting: {adjacent.X}, {adjacent.Y}");
+            didMerge = true;
+            if (dryRun) break;
+            tile.IsDeleted = true;
+            adjacent.IsDeleted = true;
+            adjacent.X -= deltaX;
+            adjacent.Y -= deltaY;
+            var newTile = new TileViewModel(new Tile(tile.X, tile.Y, tile.Value * 2)) {
+                IsMerged = true
+            };
+            newTile.UpdatePhysicalPosition();
+            Score += newTile.Value;
+            Tiles.Add(newTile);
+            // Console.WriteLine($"After: {adjacent.X}, {adjacent.Y}");
+            // Console.WriteLine($"AfterTile: {tile.X}, {tile.Y}");
+            // Console.WriteLine($"AfterNew: {newTile.X}, {newTile.Y}");
+        }
+        // Console.WriteLine("---------");
+        
+        sorted = (dir switch {
+            Direction.Left => Tiles.OrderBy(t => t.X),
+            Direction.Right => Tiles.OrderBy(t => -t.X),
+            Direction.Up => Tiles.OrderBy(t => t.Y),
+            Direction.Down => Tiles.OrderBy(t => -t.Y),
+            _ => new List<TileViewModel>().Order()
+        }).ToList();
 
         // Pushing
         foreach (var tile in sorted) {
             // Console.WriteLine($"Starting: {tile.X}, {tile.Y}");
             // Console.WriteLine($"Deltas: {deltaX}, {deltaY}");
-            while (Tiles.FirstOrDefault(t => tile.X + deltaX == t.X && tile.Y + deltaY == t.Y) == null
+            while (Tiles.FirstOrDefault(t => tile.X + deltaX == t.X && tile.Y + deltaY == t.Y && !t.IsDeleted) == null
                    && tile.X + deltaX < 4 && tile.X + deltaX >= 0 && tile.Y + deltaY < 4 && tile.Y + deltaY >= 0) {
                 didMove = true;
                 if (dryRun) break;
@@ -105,10 +132,13 @@ public partial class GameViewModel : ObservableObject {
             tile.IsMoving = true;
         }
 
-        return didMove;
+        var didChange = didMerge || didMove;
+        if (didChange && !dryRun) SpawnNew();
+        return didChange;
     }
 
     private void SpawnNew() {
+        // Console.WriteLine($"Before: {Tiles.Count(t => !t.IsDeleted)}");
         if (Tiles.Count(t => !t.IsDeleted) >= 16) {
             Console.WriteLine("out of space");
             return;
@@ -120,7 +150,7 @@ public partial class GameViewModel : ObservableObject {
         do {
             x = Random.Shared.Next(0, 4);
             y = Random.Shared.Next(0, 4);
-        } while (Tiles.Any(t => t.X == x && t.Y == y));
+        } while (Tiles.Any(t => t.X == x && t.Y == y && !t.IsDeleted));
 
         var value = Random.Shared.Next(0, 20) switch {
             0 => 4,
@@ -132,5 +162,6 @@ public partial class GameViewModel : ObservableObject {
         };
         newTile.UpdatePhysicalPosition();
         Tiles.Add(newTile);
+        // Console.WriteLine($"After: {Tiles.Count(t => !t.IsDeleted)}");
     }
 }
--- viewmodels/TileViewModel.cs
+++ viewmodels/TileViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Input;
+using System.Windows.Media;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using TwoEleven.models;
@@ -49,4 +50,29 @@ public partial class TileViewModel(Tile tile) : ObservableObject {
         PrevPhysicalX = PhysicalX;
         PrevPhysicalY = PhysicalY;
     }
+
+    public Color BackgroundColor => Value switch {
+        2 => Color.FromRgb(238, 228, 218),
+        4 => Color.FromRgb(237, 224, 200),
+        8 => Color.FromRgb(242, 177, 121),
+        16 => Color.FromRgb(245, 149, 99),
+        32 => Color.FromRgb(246, 124, 95),
+        64 => Color.FromRgb(246, 94, 59),
+        128 => Color.FromRgb(237, 207, 114),
+        256 => Color.FromRgb(237, 204, 97),
+        512 => Color.FromRgb(237, 200, 80),
+        1024 => Color.FromRgb(237, 197, 63),
+        2048 => Color.FromRgb(237, 194, 46),
+        _ => Color.FromRgb(62, 57, 51)
+    };
+    
+    public Color ForegroundColor => Value switch {
+        <= 4 => Color.FromRgb(119, 110, 101),
+        _ => Color.FromRgb(249, 246, 242)
+    };
+
+    public double FontSize => Value switch {
+        >= 1024 => 24,
+        _ => 32
+    };
 }
